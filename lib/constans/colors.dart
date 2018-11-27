8e6756127a223d91a7a2b8c9eeedd0b4f86d4eb3
juanpale <juanpale@users.noreply.github.com>
2018-11-27 19:05:58 -0300

    project basics

---
+++ lib/constans/colors.dart
@@ -0,0 +1,6 @@
+//import 'package:'
+import 'package:flutter/material.dart';
+
+const Color transparent = Color(0x00000000);
+const Color backgroundColor = Color(0xFFFFF3E0);
+const Color mainColor = Color(0xFFFF7043);
