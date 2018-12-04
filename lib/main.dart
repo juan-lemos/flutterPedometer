25b6add9b702097d6897f5de6443e4baa5011b5f
juanpale <juanpale@users.noreply.github.com>
2018-12-03 23:40:42 -0300

    basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,98 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_pedometer/Constans/colors.dart' as colors;
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_pedometer/routes.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_pedometer/Models/app_state.dart';
+import 'package:flutter_pedometer/Reducers/app_reducer.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new MainApp());
 
-class MyApp extends StatelessWidget {
+class MainApp extends StatelessWidget {
+  final String title = 'MeSuite';
+
+  MainApp();
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.yellow,
+    var store = new Store<AppState>(appReducer,
+        initialState: new AppState(), distinct: true, middleware: []);
+
+    return new StoreProvider(
+      store: store,
+      child: new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        routes: getRoutes(context, store),
+        initialRoute: '/login',
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+//TODO: OLD MAIN
+// import 'package:flutter/material.dart';
+// import 'package:flutter_pedometer/Constans/colors.dart' as colors;
 
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
+// void main() => runApp(new MyApp());
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: new ThemeData(
+//         primarySwatch: Colors.yellow,
+//       ),
+//       home: new MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+//   final String title;
 
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-        decoration: new BoxDecoration(color: colors.backgroundColor),
-        child: new SafeArea(
-            child: Column(children: <Widget>[
-          Container(
-              margin: EdgeInsets.only(left: 8, right: 0, top: 0, bottom: 4),
-              alignment: Alignment.centerRight,
-              child: new Material(
-                color: colors.transparent,
-                shape: new CircleBorder(),
-                child: new IconButton(
-                    alignment: Alignment.center,
-                    icon: new Icon(Icons.access_alarm),
-                    tooltip: 'list',
-                    onPressed: () => debugPrint('#debug print')),
-              )),
-          ListView(shrinkWrap: true, children: <Widget>[
-            Card(
-              margin: const EdgeInsets.only(left: 8, right: 8),
-              color: colors.mainColor,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[],
-              ),
-            )
-          ])
-        ])));
-  }
-}
+//   @override
+//   _MyHomePageState createState() => new _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Container(
+//         decoration: new BoxDecoration(color: colors.backgroundColor),
+//         child: new SafeArea(
+//             child: Column(children: <Widget>[
+//           Container(
+//               margin: EdgeInsets.only(left: 8, right: 0, top: 0, bottom: 4),
+//               alignment: Alignment.centerRight,
+//               child: new Material(
+//                 color: colors.transparent,
+//                 shape: new CircleBorder(),
+//                 child: new IconButton(
+//                     alignment: Alignment.center,
+//                     icon: new Icon(Icons.access_alarm),
+//                     tooltip: 'list',
+//                     onPressed: () => debugPrint('#debug print')),
+//               )),
+//           ListView(shrinkWrap: true, children: <Widget>[
+//             Card(
+//               margin: const EdgeInsets.only(left: 8, right: 8),
+//               color: colors.mainColor,
+//               child: Column(
+//                 mainAxisSize: MainAxisSize.min,
+//                 children: <Widget>[],
+//               ),
+//             )
+//           ])
+//         ])));
+//   }
+// }
---
+++ lib/routes.dart
@@ -0,0 +1,41 @@
+library routes;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_pedometer/Screens/home_page.dart';
+import 'Models/app_state.dart';
+
+void updateRoute(AppState state, BuildContext context) {
+  // if (state.currentUser != null) {
+  Navigator.pushNamed(context, '/');
+  // }
+}
+
+Map<String, WidgetBuilder> getRoutes(context, store) {
+  return {
+    '/': (BuildContext context) => new StoreBuilder<AppState>(
+          builder: (context, store) {
+            return new HomePage("");
+          },
+        ),
+    // '/login': (BuildContext context) => new StoreBuilder<AppState>(
+    //       builder: (context, store) {
+    //         return new AuthPage();
+    //       },
+    //     ),
+    // '/loading': (BuildContext context) => new StoreBuilder<AppState>(
+    //       onInit: (store) {
+    //         var oldState = store.state.copyWith();
+    //         store.onChange.listen((state) {
+    //           if (state.currentUser != oldState.currentUser) {
+    //             updateRoute(state, context);
+    //             oldState = state.copyWith();
+    //           }
+    //         });
+    //       },
+    //       builder: (context, store) {
+    //         return new LoadingPage();
+    //       },
+    //     ),
+  };
+}
