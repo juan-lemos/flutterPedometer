df0e86704f62bca9c63419d944574c4adbfa68fe
juanpale <juanpale@users.noreply.github.com>
2018-12-07 08:26:39 -0300

    modify card widget

---
+++ lib/Widgets/CustomCard.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pedometer/Constans/colors.dart' as Colors;
+
+class CustomCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        margin: const EdgeInsets.only(left: 8, right: 8),
+        color: Colors.mainColor,
+        child: Text('fgg'));
+  }
+}
