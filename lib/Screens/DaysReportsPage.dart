5fc6daf5ed025f0e5976bef5e77a4096bb0917cc
juanpale <juanpale@users.noreply.github.com>
2019-01-24 22:34:25 -0300

    resume layout days report done

--- lib/Screens/DaysReportsPage.dart
+++ lib/Screens/DaysReportsPage.dart
@@ -2,29 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:flutter_pedometer/Constants/Colors.dart' as CustomColors;
 import 'package:flutter_pedometer/Constants/Fonts.dart' as Fonts;
 import 'package:flutter_pedometer/Screens/WeekReportSubPage.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
 class DaysReportsPage extends StatelessWidget {
   DaysReportsPage();
 
   @override
   Widget build(BuildContext context) {
+    FlutterStatusbarcolor.setStatusBarWhiteForeground(false);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: CustomColors.backgroundColor,
-        title: Text('Report'),
+        title: Text('Report',
+            style: TextStyle(
+                fontSize: 20,
+                fontFamily: Fonts.secondaryFont,
+                fontWeight: FontWeight.w600,
+                color: CustomColors.strongCompleteColor)),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           tooltip: 'Air it',
           // onPressed: _airDress,
         ),
         bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(12.0),
-          child: Text('hola',
-              style: TextStyle(
-                  fontSize: 16,
-                  fontFamily: Fonts.secondaryFont,
-                  fontWeight: FontWeight.w500)),
-        ),
+            preferredSize: const Size.fromHeight(30.0),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.arrow_left),
+                  tooltip: 'Air it',
+                  // onPressed: _airDress,
+                ),
+                Text(
+                  'Oct 22-28\n2018',
+                  style: TextStyle(
+                    fontSize: 13,
+                    fontFamily: Fonts.mainFont,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_right),
+                  tooltip: 'Air it',
+                  // onPressed: _airDress,
+                ),
+              ],
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+            )),
       ),
       body: WeekReportSubPage(),
       bottomNavigationBar: BottomNavigationBar(
@@ -51,60 +77,6 @@ class DaysReportsPage extends StatelessWidget {
         fixedColor: CustomColors.strongCompleteColor,
         iconSize: 0.0,
       ),
-    );
-
-    return new Container(
-      child: SafeArea(
-          child: Center(
-              child: Column(
-        children: <Widget>[
-          AppBar(
-            backgroundColor: CustomColors.backgroundColor,
-            title: Text('Report'),
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              tooltip: 'Air it',
-              // onPressed: _airDress,
-            ),
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(48.0),
-              child: Text('hola',
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontFamily: Fonts.secondaryFont,
-                      fontWeight: FontWeight.w500)),
-            ),
-          ),
-          Flexible(
-            child: WeekReportSubPage(),
-          ),
-          BottomNavigationBar(
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                  icon: new Container(
-                    height: 14,
-                  ),
-                  title: Text('Week',
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontFamily: Fonts.secondaryFont,
-                          fontWeight: FontWeight.w500))),
-              BottomNavigationBarItem(
-                  icon: new Container(
-                    height: 14,
-                  ),
-                  title: Text('Month',
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontFamily: Fonts.secondaryFont,
-                          fontWeight: FontWeight.w500))),
-            ],
-            fixedColor: CustomColors.strongCompleteColor,
-            iconSize: 0.0,
-          ),
-        ],
-      ))),
-      color: CustomColors.backgroundColor,
     );
   }
 }
